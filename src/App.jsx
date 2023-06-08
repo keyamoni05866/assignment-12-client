a033d2f2a88775dbb33e0591ac255c8fc5599c0a
keyamoni05866 <keya05866@gmail.com>
2023-06-08 16:50:34 +0600

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src="https://assets7.lottiefiles.com/packages/lf20_2ibpmsby.json" alt="" /> */}
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -25,9 +26,12 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
   )
 }
