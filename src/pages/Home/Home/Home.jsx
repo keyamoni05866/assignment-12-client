6c68be3e978b0f2103e308defe6726ab6ec3a955
keyamoni05866 <keya05866@gmail.com>
2023-06-09 18:16:45 +0600

    react spring animation used in banner component

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-
+import { animated, useSpring } from '@react-spring/web'
 const Home = () => {
+    const springs = useSpring({
+        from: { y: 100 },
+        to: { y: 0 },
+      })
     return (
         <div className=' lg:px-24 mx-auto'>
-         <Banner></Banner>
+     <animated.div     style={{
+      
+        ...springs,
+      }}><Banner></Banner></animated.div>
+         
         </div>
     );
 };
